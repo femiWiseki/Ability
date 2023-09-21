555fcc5720b78dfe3ba41390e2703fd226515580
Femi Fashola <femifashola@wiseki.com>
2023-09-21 14:24:24 +0100

    ...

--- lib/src/features/aggregator/home/presentation/widgets/agg_home_screen.dart
+++ lib/src/features/aggregator/home/presentation/widgets/agg_home_screen.dart
@@ -5,6 +5,7 @@ import 'package:ability/src/constants/colors.dart';
 import 'package:ability/src/features/aggregator/home/presentation/widgets/refactored_widgets/agg_home_screen_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:screenshot/screenshot.dart';
 
 class AggHomeScreen extends ConsumerWidget {
   const AggHomeScreen({super.key});
@@ -13,46 +14,50 @@ class AggHomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: kAsh1,
-      body: Column(
-        children: [
-          const AggHomeScreenBar(),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(24, 15.84, 24, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Recent transaction',
-                  style: AppStyleGilroy.kFontW6.copyWith(fontSize: 20),
-                ),
-                const SizedBox(height: 5.05),
-                Container(
-                  width: double.maxFinite,
-                  height: 348,
-                  color: kWhite,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-                  child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return const Text('');
-                      // return const RecentTransactionTile();
-                    },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const AggHomeScreenBar(),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(24, 15.84, 24, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Recent transaction',
+                    style: AppStyleGilroy.kFontW6.copyWith(fontSize: 20),
                   ),
-                )
-              ],
+                  const SizedBox(height: 5.05),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      color: kWhite,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 20),
+                      child: ListView.builder(
+                        itemCount: 10,
+                        itemBuilder: (context, index) {
+                          return const Text('');
+                          // return const RecentTransactionTile();
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          // AbilityButton(
-          //     title: 'Logout',
-          //     onPressed: () {
-          //       AgentPreference.clearAccessToken().then((value) {
-          //         PageNavigator(ctx: context).nextPageOnly(
-          //             page: AgentLoginScreen(
-          //                 ValidationHelper(), AgentController()));
-          //       });
-          //     }),
-        ],
+            // AbilityButton(
+            //     title: 'Logout',
+            //     onPressed: () {
+            //       AgentPreference.clearAccessToken().then((value) {
+            //         PageNavigator(ctx: context).nextPageOnly(
+            //             page: AgentLoginScreen(
+            //                 ValidationHelper(), AgentController()));
+            //       });
+            //     }),
+          ],
+        ),
       ),
     );
   }
